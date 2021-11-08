a522a1364a595bb54fc1d02dd0a791ba0429d73e
roblabla <unfiltered@roblab.la>
2021-11-08 15:47:12 +0100

    Avoid shell32.dll dependency to parse cmdline

--- src/windows/process.rs
+++ src/windows/process.rs
@@ -8,13 +8,16 @@ use crate::{DiskUsage, Pid, ProcessExt, ProcessStatus, Signal};
 
 use std::ffi::OsString;
 use std::fmt;
+use std::iter;
+use std::marker::PhantomData;
 use std::mem::{size_of, zeroed, MaybeUninit};
+use std::num::NonZeroU16;
 use std::ops::Deref;
 use std::os::windows::ffi::OsStringExt;
 use std::os::windows::process::CommandExt;
 use std::path::{Path, PathBuf};
 use std::process;
-use std::ptr::null_mut;
+use std::ptr::{NonNull, null_mut};
 use std::str;
 
 use libc::{c_void, memcpy};
@@ -220,7 +223,7 @@ impl Process {
             let exe = unsafe { get_exe(handle, h_mod) };
             let mut root = exe.clone();
             root.pop();
-            let (cmd, environ, cwd) = match unsafe { get_process_params(handle) } {
+            let (cmd, environ, cwd) = match unsafe { get_process_params(handle, &exe) } {
                 Ok(args) => args,
                 Err(_e) => {
                     sysinfo_debug!("Failed to get process parameters: {}", _e);
@@ -288,7 +291,7 @@ impl Process {
             let exe = get_exe(process_handler, h_mod);
             let mut root = exe.clone();
             root.pop();
-            let (cmd, environ, cwd) = match get_process_params(process_handler) {
+            let (cmd, environ, cwd) = match get_process_params(process_handler, &exe) {
                 Ok(args) => args,
                 Err(_e) => {
                     sysinfo_debug!("Failed to get process parameters: {}", _e);
@@ -489,26 +492,184 @@ unsafe fn ph_query_process_variable_size(
     Some(buffer)
 }
 
-unsafe fn get_cmdline_from_buffer(buffer: *const u16) -> Vec<String> {
-    // Get argc and argv from the command line
-    let mut argc = MaybeUninit::<i32>::uninit();
-    let argv_p = winapi::um::shellapi::CommandLineToArgvW(buffer, argc.as_mut_ptr());
-    if argv_p.is_null() {
-        return Vec::new();
+unsafe fn get_cmdline_from_buffer(buffer: *const u16, exe: &Path) -> Vec<String> {
+    parse_lp_cmd_line(WStrUnits::new(buffer), || exe.as_os_str().to_os_string())
+        .into_iter()
+        .map(|v| v.to_string_lossy().into_owned())
+        .collect()
+}
+
+// Taken from https://github.com/rust-lang/rust/blob/1cf8fdd4f0be26bcfa9e3b1e10d4bf80107ba492/library/std/src/sys/windows/args.rs#L35-L155
+/// Implements the Windows command-line argument parsing algorithm.
+///
+/// Microsoft's documentation for the Windows CLI argument format can be found at
+/// <https://docs.microsoft.com/en-us/cpp/cpp/main-function-command-line-args?view=msvc-160#parsing-c-command-line-arguments>
+///
+/// A more in-depth explanation is here:
+/// <https://daviddeley.com/autohotkey/parameters/parameters.htm#WIN>
+///
+/// Windows includes a function to do command line parsing in shell32.dll.
+/// However, this is not used for two reasons:
+///
+/// 1. Linking with that DLL causes the process to be registered as a GUI application.
+/// GUI applications add a bunch of overhead, even if no windows are drawn. See
+/// <https://randomascii.wordpress.com/2018/12/03/a-not-called-function-can-cause-a-5x-slowdown/>.
+///
+/// 2. It does not follow the modern C/C++ argv rules outlined in the first two links above.
+///
+/// This function was tested for equivalence to the C/C++ parsing rules using an
+/// extensive test suite available at
+/// <https://github.com/ChrisDenton/winarg/tree/std>.
+fn parse_lp_cmd_line<'a, F: Fn() -> OsString>(
+    lp_cmd_line: Option<WStrUnits<'a>>,
+    exe_name: F,
+) -> Vec<OsString> {
+    const BACKSLASH: NonZeroU16 = unsafe { NonZeroU16::new_unchecked(b'\\' as u16) };
+    const QUOTE: NonZeroU16 = unsafe { NonZeroU16::new_unchecked(b'"' as u16) };
+    const TAB: NonZeroU16 = unsafe { NonZeroU16::new_unchecked(b'\t' as u16) };
+    const SPACE: NonZeroU16 = unsafe { NonZeroU16::new_unchecked(b' ' as u16) };
+
+    let mut ret_val = Vec::new();
+    // If the cmd line pointer is null or it points to an empty string then
+    // return the name of the executable as argv[0].
+    if lp_cmd_line.as_ref().and_then(|cmd| cmd.peek()).is_none() {
+        ret_val.push(exe_name());
+        return ret_val;
     }
-    let argc = argc.assume_init();
-    let argv = std::slice::from_raw_parts(argv_p, argc as usize);
+    let mut code_units = lp_cmd_line.unwrap();
 
-    let mut res = Vec::new();
-    for arg in argv {
-        let len = libc::wcslen(*arg);
-        let str_slice = std::slice::from_raw_parts(*arg, len);
-        res.push(String::from_utf16_lossy(str_slice));
+    // The executable name at the beginning is special.
+    let mut in_quotes = false;
+    let mut cur = Vec::new();
+    for w in &mut code_units {
+        match w {
+            // A quote mark always toggles `in_quotes` no matter what because
+            // there are no escape characters when parsing the executable name.
+            QUOTE => in_quotes = !in_quotes,
+            // If not `in_quotes` then whitespace ends argv[0].
+            SPACE | TAB if !in_quotes => break,
+            // In all other cases the code unit is taken literally.
+            _ => cur.push(w.get()),
+        }
     }
+    // Skip whitespace.
+    code_units.advance_while(|w| w == SPACE || w == TAB);
+    ret_val.push(OsString::from_wide(&cur));
 
-    winapi::um::winbase::LocalFree(argv_p as *mut _);
+    // Parse the arguments according to these rules:
+    // * All code units are taken literally except space, tab, quote and backslash.
+    // * When not `in_quotes`, space and tab separate arguments. Consecutive spaces and tabs are
+    // treated as a single separator.
+    // * A space or tab `in_quotes` is taken literally.
+    // * A quote toggles `in_quotes` mode unless it's escaped. An escaped quote is taken literally.
+    // * A quote can be escaped if preceded by an odd number of backslashes.
+    // * If any number of backslashes is immediately followed by a quote then the number of
+    // backslashes is halved (rounding down).
+    // * Backslashes not followed by a quote are all taken literally.
+    // * If `in_quotes` then a quote can also be escaped using another quote
+    // (i.e. two consecutive quotes become one literal quote).
+    let mut cur = Vec::new();
+    let mut in_quotes = false;
+    while let Some(w) = code_units.next() {
+        match w {
+            // If not `in_quotes`, a space or tab ends the argument.
+            SPACE | TAB if !in_quotes => {
+                ret_val.push(OsString::from_wide(&cur[..]));
+                cur.truncate(0);
 
-    res
+                // Skip whitespace.
+                code_units.advance_while(|w| w == SPACE || w == TAB);
+            }
+            // Backslashes can escape quotes or backslashes but only if consecutive backslashes are followed by a quote.
+            BACKSLASH => {
+                let backslash_count = code_units.advance_while(|w| w == BACKSLASH) + 1;
+                if code_units.peek() == Some(QUOTE) {
+                    cur.extend(iter::repeat(BACKSLASH.get()).take(backslash_count / 2));
+                    // The quote is escaped if there are an odd number of backslashes.
+                    if backslash_count % 2 == 1 {
+                        code_units.next();
+                        cur.push(QUOTE.get());
+                    }
+                } else {
+                    // If there is no quote on the end then there is no escaping.
+                    cur.extend(iter::repeat(BACKSLASH.get()).take(backslash_count));
+                }
+            }
+            // If `in_quotes` and not backslash escaped (see above) then a quote either
+            // unsets `in_quote` or is escaped by another quote.
+            QUOTE if in_quotes => match code_units.peek() {
+                // Two consecutive quotes when `in_quotes` produces one literal quote.
+                Some(QUOTE) => {
+                    cur.push(QUOTE.get());
+                    code_units.next();
+                }
+                // Otherwise set `in_quotes`.
+                Some(_) => in_quotes = false,
+                // The end of the command line.
+                // Push `cur` even if empty, which we do by breaking while `in_quotes` is still set.
+                None => break,
+            },
+            // If not `in_quotes` and not BACKSLASH escaped (see above) then a quote sets `in_quote`.
+            QUOTE => in_quotes = true,
+            // Everything else is always taken literally.
+            _ => cur.push(w.get()),
+        }
+    }
+    // Push the final argument, if any.
+    if !cur.is_empty() || in_quotes {
+        ret_val.push(OsString::from_wide(&cur[..]));
+    }
+    ret_val
+}
+
+
+/// A safe iterator over a LPWSTR
+/// (aka a pointer to a series of UTF-16 code units terminated by a NULL).
+struct WStrUnits<'a> {
+    // The pointer must never be null...
+    lpwstr: NonNull<u16>,
+    // ...and the memory it points to must be valid for this lifetime.
+    lifetime: PhantomData<&'a [u16]>,
+}
+impl WStrUnits<'_> {
+    /// Create the iterator. Returns `None` if `lpwstr` is null.
+    ///
+    /// SAFETY: `lpwstr` must point to a null-terminated wide string that lives
+    /// at least as long as the lifetime of this struct.
+    unsafe fn new(lpwstr: *const u16) -> Option<Self> {
+        Some(Self { lpwstr: NonNull::new(lpwstr as _)?, lifetime: PhantomData })
+    }
+    fn peek(&self) -> Option<NonZeroU16> {
+        // SAFETY: It's always safe to read the current item because we don't
+        // ever move out of the array's bounds.
+        unsafe { NonZeroU16::new(*self.lpwstr.as_ptr()) }
+    }
+    /// Advance the iterator while `predicate` returns true.
+    /// Returns the number of items it advanced by.
+    fn advance_while<P: FnMut(NonZeroU16) -> bool>(&mut self, mut predicate: P) -> usize {
+        let mut counter = 0;
+        while let Some(w) = self.peek() {
+            if !predicate(w) {
+                break;
+            }
+            counter += 1;
+            self.next();
+        }
+        counter
+    }
+}
+impl Iterator for WStrUnits<'_> {
+    // This can never return zero as that marks the end of the string.
+    type Item = NonZeroU16;
+    fn next(&mut self) -> Option<NonZeroU16> {
+        // SAFETY: If NULL is reached we immediately return.
+        // Therefore it's safe to advance the pointer after that.
+        unsafe {
+            let next = self.peek()?;
+            self.lpwstr = NonNull::new_unchecked(self.lpwstr.as_ptr().add(1));
+            Some(next)
+        }
+    }
 }
 
 unsafe fn get_region_size(handle: HANDLE, ptr: LPVOID) -> Result<usize, &'static str> {
@@ -581,6 +742,7 @@ impl_RtlUserProcessParameters!(RTL_USER_PROCESS_PARAMETERS);
 
 unsafe fn get_process_params(
     handle: HANDLE,
+    exe: &Path
 ) -> Result<(Vec<String>, Vec<String>, PathBuf), &'static str> {
     if !cfg!(target_pointer_width = "64") {
         return Err("Non 64 bit targets are not supported");
@@ -644,7 +806,7 @@ unsafe fn get_process_params(
 
         let proc_params = proc_params.assume_init();
         return Ok((
-            get_cmd_line(&proc_params, handle),
+            get_cmd_line(&proc_params, handle, exe),
             get_proc_env(&proc_params, handle),
             get_cwd(&proc_params, handle),
         ));
@@ -677,7 +839,7 @@ unsafe fn get_process_params(
     }
     let proc_params = proc_params.assume_init();
     Ok((
-        get_cmd_line(&proc_params, handle),
+        get_cmd_line(&proc_params, handle, exe),
         get_proc_env(&proc_params, handle),
         get_cwd(&proc_params, handle),
     ))
@@ -702,9 +864,9 @@ unsafe fn null_terminated_wchar_to_string(slice: &[u16]) -> String {
     }
 }
 
-fn get_cmd_line_old<T: RtlUserProcessParameters>(params: &T, handle: HANDLE) -> Vec<String> {
+fn get_cmd_line_old<T: RtlUserProcessParameters>(params: &T, handle: HANDLE, exe: &Path) -> Vec<String> {
     match params.get_cmdline(handle) {
-        Ok(buffer) => unsafe { get_cmdline_from_buffer(buffer.as_ptr()) },
+        Ok(buffer) => unsafe { get_cmdline_from_buffer(buffer.as_ptr(), exe) },
         Err(_e) => {
             sysinfo_debug!("get_cmd_line_old failed to get data: {}", _e);
             Vec::new()
@@ -713,24 +875,24 @@ fn get_cmd_line_old<T: RtlUserProcessParameters>(params: &T, handle: HANDLE) -> 
 }
 
 #[allow(clippy::cast_ptr_alignment)]
-fn get_cmd_line_new(handle: HANDLE) -> Vec<String> {
+fn get_cmd_line_new(handle: HANDLE, exe: &Path) -> Vec<String> {
     unsafe {
         if let Some(buffer) = ph_query_process_variable_size(handle, ProcessCommandLineInformation)
         {
             let buffer = (*(buffer.as_ptr() as *const UNICODE_STRING)).Buffer;
 
-            get_cmdline_from_buffer(buffer)
+            get_cmdline_from_buffer(buffer, exe)
         } else {
             vec![]
         }
     }
 }
 
-fn get_cmd_line<T: RtlUserProcessParameters>(params: &T, handle: HANDLE) -> Vec<String> {
+fn get_cmd_line<T: RtlUserProcessParameters>(params: &T, handle: HANDLE, exe: &Path) -> Vec<String> {
     if *WINDOWS_8_1_OR_NEWER {
-        get_cmd_line_new(handle)
+        get_cmd_line_new(handle, exe)
     } else {
-        get_cmd_line_old(params, handle)
+        get_cmd_line_old(params, handle, exe)
     }
 }
 
